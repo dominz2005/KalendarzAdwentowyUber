2f08813ceee46c80c809ac06704f0c0441c52557
dominz2005 <73389814+dominz2005@users.noreply.github.com>
2025-12-10 16:20:59 +0100

    Małe zmiany tekstowe + ekran końca

--- js/date.js
+++ js/date.js
@@ -1,5 +1,21 @@
-const currentSpan = document.querySelector(".currentDate");
+const normalScreen = document.querySelector(".normalScreen");
+const arrowDown = document.querySelector(".arrowDown");
+const mainText = document.querySelector(".mainText");
 
 const date = new Date();
 
-currentSpan.innerText = `${date.getDate()}.12`;
+if (date.getDate() >= 25) hideEverything();
+
+window.addEventListener("keypress", event => {
+    if(!event.ctrlKey) return;
+    if(!event.code == "KeyO") return;
+
+    hideEverything();
+});
+
+function hideEverything() {
+    normalScreen.remove();
+    arrowDown.classList.add("opacity-0");
+    arrowDown.classList.add("pointer-events-none");
+    mainText.innerText = "Dziękujemy i życzymy Szczęśliwego Nowego Roku.";
+}
